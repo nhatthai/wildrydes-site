c1eb6ce141861ff1725cbdd7e8d0d1a9c6337806
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-01-28 10:12:48 +0700

    Update config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'ap-southeast-1_W4Jk7y29W',
+        userPoolClientId: '3a74un0kle4olm2pjmj5ee3q4b',
+        region: 'ap-southeast-1'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
